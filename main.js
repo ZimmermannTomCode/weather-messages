65c41752191376652cfabd66731d0baca41de041
Tom <TomZimm3rmann@yahoo.de>
2021-12-14 19:29:25 +0100

    finished

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ const item = ["a winter jacket", "an umbrella", "suncream", "a hoodie"];
 let dayNumber = Math.floor(Math.random() * 2);
 let weatherNumber = Math.floor(Math.random() * 4);
 let itemNumber = Math.floor(Math.random() * 4);
+
+finalMessage = [day[dayNumber] + " it is going to be " + weather[weatherNumber] + " we would recommed to go outside with " + item[itemNumber]];
+console.log(finalMessage);
