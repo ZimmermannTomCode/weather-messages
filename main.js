0167c150b187b714141031aff27b625157f08680
Tom <TomZimm3rmann@yahoo.de>
2021-12-14 19:25:02 +0100

    filled arrays + random Numbers

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let finalMessage = [];
-const day = [];
-const weather = [];
-const item = [];
+const day = ["today", "tomorrow"];
+const weather = ["sunny", "rainy", "cloudy", "clear"];
+const item = ["a winter jacket", "an umbrella", "suncream", "a hoodie"];
+let dayNumber = Math.floor(Math.random() * 2);
+let weatherNumber = Math.floor(Math.random() * 4);
+let itemNumber = Math.floor(Math.random() * 4);
